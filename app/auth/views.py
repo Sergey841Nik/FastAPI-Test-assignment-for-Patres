782f3afb5717c80202c67022dc0e25d36e138eaa
Сергей <nikitos841@yandex.ru>
2025-01-29 20:56:02 +0400

    add update info user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,12 @@
 from logging import getLogger
+from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends, HTTPException, Response, status, Form
 
 
 from ..models.db_helper import db_helper
 from .schemes import EmailModel, UserRegister, UserAddDB, UserAuth, UserInfo
+
 # from .crud import find_one_or_none_users, add_users, get_all_users
 from .crud import UsersDAO
 from .auth_jwt import validate_auth_user, create_access_token
@@ -54,6 +56,23 @@ async def auth_user(
 async def logout_user(response: Response):
     response.delete_cookie(key="access_token")
     return {"message": "Пользователь успешно вышел из системы"}
+
+
+@router.put("/change_info_user_self/")
+async def change_role(
+    user: UserRegister,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    user_data=Depends(get_current_user),
+):
+    user_dict = user.model_dump()
+    del user_dict["confirm_password"]
+
+    await UsersDAO.update(
+        session=session, user=user_data, values=UserRegister(**user_dict)
+    )
+    await session.commit()
+
+    return {"message": "Данные пользователя изменены"}
 
 
 @router.get("/me/")
